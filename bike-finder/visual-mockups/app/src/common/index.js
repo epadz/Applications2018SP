fd528704b94204eaf0416d675d3eea41a0f7befd
epadz <e.padzensky@comcast.net>
2018-02-19 20:38:46 -0600

    clickable prototype

--- bike-finder/visual-mockups/app/src/common/index.js
+++
@@ -1,7 +0,0 @@
-import ColorDot from './colorDot';
-import DropDown from './dropDown';
-import StarRating from './starRating';
-import Button from './button';
-import Number from './number';
-
-export { Button, Number, ColorDot, DropDown, StarRating};
